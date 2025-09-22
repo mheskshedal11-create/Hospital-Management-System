dc55a714ef8d4da7ef313d30292ce7f49bcb2687
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-09-22 21:30:37 +0545

    mongo connect successfully

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import fileUpload from 'express-fileupload'
+import dbConnection from './database/dbConnection.js'
 dotenv.config()
 
 const app = express()
+
 import cookieParse from 'cookie-parser'
 app.use(cors({
     origin: [process.env.FRONTEND_URL, process.env.DASHBOARD_URL],
@@ -14,7 +16,7 @@ app.use(cors({
 app.use(cookieParse())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-
+dbConnection()
 app.use(fileUpload({
     useTempFiles: true,
     tempFileDir: '/tmp/'
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 
 import app from "./app.js";
+import { v2 as cloudinary } from 'cloudinary'
 
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secrate: process.env.CLOUDINARY_API_SECRATE
+})
 const PORT = process.env.PORT || 8000
-
 app.listen(PORT, (err) => {
     if (err) {
         console.error("Error starting server:", err);
