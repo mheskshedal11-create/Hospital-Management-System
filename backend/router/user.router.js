f349b36dbe0a622b47b76c628e465d0e72cdf236
mahesh kumar chhedal <mheskshedal11@gmail.com>
2025-09-23 19:49:57 +0545

    user login

--- backend/router/user.router.js
+++ backend/router/user.router.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { patientRegister } from '../controller/user.controller.js'
+import { login, patientRegister } from '../controller/user.controller.js'
 const userRouter = express.Router()
 
 userRouter.post('/patient/register', patientRegister)
+userRouter.post('/patient/login', login)
 
 export default userRouter
